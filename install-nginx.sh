82b139e67f8a4ea8a4e88cd545b08aace05455eb
Serarinne <160508714+Serarinne@users.noreply.github.com>
2025-09-21 17:48:21 +0700

    Update install-nginx.sh

--- install-nginx.sh
+++ install-nginx.sh
@@ -53,21 +53,6 @@ server {
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     }
-    
-    # Lokasi untuk VMess TLS WebSocket
-    location /vmess-tls {
-        if (\$http_upgrade != "websocket") {
-            return 404;
-        }
-        proxy_pass http://127.0.0.1:7002;
-        proxy_redirect off;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection "upgrade";
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-    }
 }
 EOF
 
