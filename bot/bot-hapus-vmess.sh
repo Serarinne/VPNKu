3486631e80c7fa20153df011ce28c4ba749d28eb
Serarinne <160508714+Serarinne@users.noreply.github.com>
2025-09-26 18:50:23 +0700

    Update bot-hapus-vmess.sh

--- bot/bot-hapus-vmess.sh
+++ bot/bot-hapus-vmess.sh
@@ -2,6 +2,6 @@
 CONFIG_PATH="/usr/local/etc/xray/config.json"
 CLIENT_EMAIL=$1
 
-sed -i "/# Akun-$CLIENT_EMAIL/d" /usr/local/etc/xray/config.json
+sed -i "/# Akun-${CLIENT_EMAIL}/d" /usr/local/etc/xray/config.json
 echo "1"
 systemctl restart xray
