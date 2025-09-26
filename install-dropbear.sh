6f81b4043611b887c79623d586613488250d4489
Serarinne <160508714+Serarinne@users.noreply.github.com>
2025-09-26 23:30:06 +0700

    Update install-dropbear.sh

--- install-dropbear.sh
+++ install-dropbear.sh
@@ -9,8 +9,7 @@ apt install dropbear -y >/dev/null 2>&1
 echo 'NO_START=0' >> /etc/default/dropbear
 echo 'DROPBEAR_PORT=143' >> /etc/default/dropbear
 echo 'DROPBEAR_EXTRA_ARGS="-p 109"' >> /etc/default/dropbear
-systemctl daemon-reload >/dev/null 2>&1
-systemctl start dropbear >/dev/null 2>&1
+
 systemctl restart dropbear >/dev/null 2>&1
 echo ""
 echo "✅ Instalasi & Konfigurasi Dropbear Selesai!"
