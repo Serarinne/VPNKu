3ffd49e919601c35234eafad577a576aa1c21f93
Serarinne <160508714+Serarinne@users.noreply.github.com>
2025-09-27 01:41:09 +0700

    Update bot-tambah-ssh.sh

--- bot/bot-tambah-ssh.sh
+++ bot/bot-tambah-ssh.sh
@@ -2,13 +2,13 @@
 USERNAME=$1
 PASSWORD=$2
 
-if id "$USERNAME" &>/dev/null; then
+if id "${USERNAME}" &>/dev/null; then
     echo "Pengguna '${USERNAME}' sudah ada. Silakan pilih nama lain."
     exit 1
 fi
 
-adduser --quiet --disabled-password --gecos "" "$USERNAME"
+adduser --quiet --disabled-password --gecos "" "${USERNAME}"
 
-echo "$USERNAME:$PASSWORD" | chpasswd
+echo "${USERNAME}:${PASSWORD}" | chpasswd
 
 echo "1"
